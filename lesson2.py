be4ba175086161d8036f1a6768c95a3c724766bb
kodovoi <kodovoi24@gmail.com>
2024-10-13 19:06:29 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+the_namber_of_completed_dz = 12
+hours_spent = 1.5
+name_kurs = 'Python'
+time_for_one_task = 0.125
+print('курс:',name_kurs,'всего задач:',the_namber_of_completed_dz,'затрачено часов:',hours_spent,'среднее время выполнения:',time_for_one_task)
---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Alexandr'
+print(name)
+age = 39
+print(age)
+new_age = 39+13
+print(new_age)
+is_student = 'true'
+print(is_student)
