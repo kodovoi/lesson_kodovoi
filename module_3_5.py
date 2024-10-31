02a63f00983f838d26af4e0d6bef40963e47d39f
kodovoi <kodovoi24@gmail.com>
2024-10-31 21:07:51 +0300

    Рекурсия

--- module_3_5.py
+++ module_3_5.py
@@ -1,12 +1,11 @@
 def get_multiplied_digits(number):
     str_number = str(number)
-
+    first = int(str_number[0])
     if len(str_number) > 1:
-        first = int(str_number[0])
         return first * get_multiplied_digits(int(str_number[1:]))
     else:
         return int(str_number)
 
 
-result1 = get_multiplied_digits(40203) # 4*(2*3)
-print(result1)
+result = get_multiplied_digits(40203) # 4*(2*3)
+print(result)
