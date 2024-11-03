3dcb771f31cb50f1da0d0ef74c62d2af91955979
kodovoi <kodovoi24@gmail.com>
2024-11-03 21:24:08 +0300

    калькулятор

--- calc.py
+++ calc.py
@@ -1,17 +1,53 @@
 import tkinter as tk
 
-window = tk.Tk()
 
+def get_values():
+    num1 = int(number1_entry.get())
+    num2 = int(number2_entry.get())
+    return num1, num2
+
+
+def insert_vales(value):
+    answer_entry.delete(0, 'end')
+    answer_entry.insert(0, value)
+
+
+def add():
+    num1, num2 = get_values()
+    res = num1 + num2
+    insert_vales(res)
+
+
+def sub():
+    num1, num2 = get_values()
+    res = num1 - num2
+    insert_vales(res)
+
+
+def div():
+    num1, num2 = get_values()
+    res = num1 / num2
+    insert_vales(res)
+
+
+def mul():
+    num1, num2 = get_values()
+    res = num1 * num2
+    insert_vales(res)
+
+
+
+window = tk.Tk()
 window.title('Калькулятор')
 window.geometry("350x350")
 window.resizable(False,False)# Ограничиваем размер окна
-button_add = tk.Button(window, text="+", width = 5, height = 6)
+button_add = tk.Button(window, text="+", width = 5, height = 6, command=add)
 button_add.place(x = 50, y = 140)
-button_sub = tk.Button(window, text="-", width=5, height=6)
+button_sub = tk.Button(window, text="-", width=5, height=6, command=sub)
 button_sub.place(x = 110, y = 140)
-button_mul = tk.Button(window, text="*", width=5, height=6)
+button_mul = tk.Button(window, text="*", width=5, height=6, command=div)
 button_mul.place(x = 200, y = 140)
-button_div = tk.Button(window, text="/", width=5, height=6)
+button_div = tk.Button(window, text="/", width=5, height=6, command=mul)
 button_div.place(x = 260, y = 140)
 number1_entry = tk.Entry(window, width=13)
 number1_entry.place(x = 50, y = 75)
