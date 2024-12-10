0fabbd2d3d7e439e353c75eceeab6b26c424b879
kodovoi <kodovoi24@gmail.com>
2024-12-10 22:13:37 +0300

    Работа с файлами

---
+++ module7/module_7_1.py
@@ -0,0 +1,50 @@
+
+
+
+class Product:
+    def __init__(self, name: str, weight: float, category: str):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category} '
+
+
+class Shop:
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        products = file.read()
+        file.close()
+        return products
+
+    def add(self, *products):
+        cur_product = self.get_products()
+        file = open(self.__file_name, 'a')
+
+
+        for product in products:
+            if str(product) not in cur_product:
+
+                file.write(str(product) + '\n')
+                cur_product += str(product) + '\n'
+            else:
+                product_name = str(product).split(', ')[0]
+                print(f'Продукт {product_name} уже есть в магазине')
+        file.close()
+
+
+
+s1 = Shop()
+
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)
+
+s1.add(p1, p2, p3)
+print(s1.get_products())
