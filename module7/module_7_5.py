d4ce549298f89fa29f25ba5e65b93c2ac20b1727
kodovoi <kodovoi24@gmail.com>
2024-12-10 22:39:44 +0300

    Работа с файлами

---
+++ module7/module_7_5.py
@@ -0,0 +1,27 @@
+import os
+import time
+
+# Указываем путь к каталогу, который будем обходить
+directory = "."
+
+# Используем os.walk для обхода каталога
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        # Формируем полный путь к файлу
+        filepath = os.path.join(root, file)
+
+        # Получаем время последнего изменения файла
+        filetime = os.path.getmtime(filepath)
+
+        # Форматируем время в нужный формат
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+
+        # Получаем размер файла
+        filesize = os.path.getsize(filepath)
+
+        # Получаем родительскую директорию файла
+        parent_dir = os.path.dirname(filepath)
+
+        # Выводим информацию о файле
+        print(
+            f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
