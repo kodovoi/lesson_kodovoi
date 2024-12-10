4089494102effb36f06a654f85a5c906354ef6b0
kodovoi <kodovoi24@gmail.com>
2024-12-10 22:30:26 +0300

    Работа с файлами

---
+++ module7/module_7_3.py
@@ -0,0 +1,44 @@
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        for file_name in self.file_names:
+            with open(file_name, 'r', encoding='utf-8') as file:
+                words = []
+                for line in file:
+                    line = line.lower()
+                    # Удаляем знаки препинания
+                    line = ''.join(char for char in line if char.isalnum() or char.isspace())
+                    words.extend(line.split())
+                all_words[file_name] = words
+        return all_words
+
+    def find(self, word):
+        word = word.lower()
+        result = {}
+        all_words = self.get_all_words()
+        for file_name, words in all_words.items():
+            if word in words:
+                position = words.index(word)
+                result[file_name] = position
+        return result
+
+    def count(self, word):
+        word = word.lower()
+        result = {}
+        all_words = self.get_all_words()
+        for file_name, words in all_words.items():
+            count = words.count(word)
+            if count > 0:
+                result[file_name] = count
+        return result
+
+# Пример использования
+finder2 = WordsFinder('test_file.txt')
+
+print(finder2.get_all_words())
+print(finder2.find('text'))
+print(finder2.count('text'))
+
