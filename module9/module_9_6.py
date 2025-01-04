201182650124b4fd967f3bd663174084702c57ba
kodovoi <kodovoi24@gmail.com>
2025-01-04 15:14:22 +0300

    генераторы

--- module9/module_9_6.py
+++ module9/module_9_6.py
@@ -1,11 +1,18 @@
 def all_variants(text):
     length = len(text)
-    for start in range(length):
-        for end in range(start + 1, length + 1):
-            yield text[start:end]
+
+    for i in range(length):
+        yield text[i]
+
+    for i in range(length - 1):
+        yield text[i:i+2]
+
+    if length >= 3:
+        yield text
 
 # Пример использования функции
 a = all_variants("abc")
 
 for i in a:
     print(i)
+
